60b1b8f1a021b3c8a31981c5440eb9c40b769eab
Shreegayatri <gayatridevi.k@manurevasolutions.com>
2024-01-17 11:13:59 +0530

    fix inital commit

---
+++ Curd/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return <View></View>;
+}
+
+export default App;
